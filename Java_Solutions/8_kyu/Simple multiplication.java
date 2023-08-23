5c4a94fe4324b0858c533d56be59bcd90f29a81f
6cypT0r <79215914+6cypT0r@users.noreply.github.com>
2023-08-23 19:32:23 +0300

    Create Simple multiplication.java

---
+++ Java_Solutions/8_kyu/Simple multiplication.java
@@ -0,0 +1,13 @@
+public class Sid {
+    public static int simpleMultiplication(int n) {
+        //your code here
+        if(n%2==0)
+          {
+          return n*8;
+        }
+      else
+        {
+        return n*9;
+      }
+    }
+}
