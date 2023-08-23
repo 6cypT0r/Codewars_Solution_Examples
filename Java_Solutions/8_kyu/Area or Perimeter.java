cf7cef1f5e5b2eaedd28319ed03932c969980d11
6cypT0r <79215914+6cypT0r@users.noreply.github.com>
2023-08-23 19:32:56 +0300

    Create Area or Perimeter.java

---
+++ Java_Solutions/8_kyu/Area or Perimeter.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static int areaOrPerimeter (int l, int w) {
+        // code away...
+       if(l==w)
+    {
+    return (int) Math.pow(l,2);
+  }
+  else{
+  return (l+w)*2;}
+    }
+}
