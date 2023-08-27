b2fa0b56699677ef8d012df3a962938c318a0396
6cypT0r <79215914+6cypT0r@users.noreply.github.com>
2023-08-27 09:41:37 +0300

    Create Convert a Boolean to a String.java

---
+++ Java_Solutions/8_kyu/Convert a Boolean to a String.java
@@ -0,0 +1,14 @@
+public class BooleanToString {
+  public static String convert(boolean b)
+  {
+    if(b==true)
+        {
+            return "true";
+        }
+        else
+        {
+            return "false";
+        }
+  }
+
+}
