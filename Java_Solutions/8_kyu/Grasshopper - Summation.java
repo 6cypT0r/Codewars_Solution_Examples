83b58a9e1c47db426d2dfe676c9fad058e0b5661
6cypT0r <79215914+6cypT0r@users.noreply.github.com>
2023-08-29 15:40:02 +0300

    Create Grasshopper - Summation.java

---
+++ Java_Solutions/8_kyu/Grasshopper - Summation.java
@@ -0,0 +1,12 @@
+public class GrassHopper {
+
+    public static int summation(int n) {
+
+        int b=0;
+        for (int i =1; i<n+1;i++)
+          {
+          b+=i;
+        }
+        return b;
+    }
+}
