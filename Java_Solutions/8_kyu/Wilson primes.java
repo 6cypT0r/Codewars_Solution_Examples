499070cc5db94cc21deec25e3afeeeb68004e3ce
6cypT0r <79215914+6cypT0r@users.noreply.github.com>
2023-08-31 08:06:19 +0300

    Create Wilson primes.java

---
+++ Java_Solutions/8_kyu/Wilson primes.java
@@ -0,0 +1,12 @@
+public class WilsonPrime {
+	public static boolean am_i_wilson(double n) {
+    //your code here
+    if (n==3 || n ==5 || n ==563)
+      {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
