abd088885218f7d9aa430060aba2f3dd0eaca584
6cypT0r <79215914+6cypT0r@users.noreply.github.com>
2023-08-31 08:04:11 +0300

    Create Find the Integral.java

---
+++ Java_Solutions/8_kyu/Find the Integral.java
@@ -0,0 +1,8 @@
+public class Kata {
+
+    public static String integrate(int coefficient, int exponent) {
+        // TODO implement the integration
+        return ""+(coefficient/(exponent+1))+"x^"+(exponent+1);
+    }
+  
+}
