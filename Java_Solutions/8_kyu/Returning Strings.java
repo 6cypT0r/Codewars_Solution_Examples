c213495f88918351e84465380f2f9ff49beef4e6
6cypT0r <79215914+6cypT0r@users.noreply.github.com>
2023-09-01 13:22:55 +0300

    Create Returning Strings.java

---
+++ Java_Solutions/8_kyu/Returning Strings.java
@@ -0,0 +1,9 @@
+public class Kata
+{
+  public static String greet(String name)
+  {
+   StringBuilder sb = new StringBuilder();
+    sb.append("Hello, "+name+" how are you doing today?");
+    return sb.toString();
+  }
+}
