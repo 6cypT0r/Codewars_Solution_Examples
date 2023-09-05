9b9fccac78503d88bac480bad3aec98f26f86f13
6cypT0r <79215914+6cypT0r@users.noreply.github.com>
2023-09-05 20:18:21 +0300

    Create String repeat.java

---
+++ Java_Solutions/8_kyu/String repeat.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static String repeatStr(final int repeat, final String string) {
+      StringBuilder sb = new StringBuilder();
+      for ( int i = 0; i< repeat; i++)
+        {
+        sb.append(string);
+      }
+        return sb.toString();
+    }
+}
