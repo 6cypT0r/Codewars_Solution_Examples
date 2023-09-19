7dcbef4af241a20fdfa742e5c99306ca8b1a2217
6cypT0r <79215914+6cypT0r@users.noreply.github.com>
2023-09-19 21:30:41 +0300

    Create Reversed Strings.java

---
+++ Java_Solutions/8_kyu/Reversed Strings.java
@@ -0,0 +1,11 @@
+public class Kata {
+
+  public static String solution(String str) {
+    // Your code here...
+    StringBuilder sb =new StringBuilder();
+    sb.append(str);
+    sb.reverse();
+    return sb.toString();
+  }
+
+}
